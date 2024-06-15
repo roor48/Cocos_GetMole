e87bd4c42c41dd694be82179c502297d325ae376
gunggme <ekfrifajfl2019@gmail.com>
2024-06-15 09:00:00 +0900

    화면 이미지 수정

--- assets/Scripts/ScreenResolution.ts
+++ assets/Scripts/ScreenResolution.ts
@@ -3,9 +3,15 @@ const { ccclass, property } = _decorator;
 
 @ccclass('ScreenResolution')
 export class ScreenResolution extends Component {
+    @property(Number)
+    public screenWidth : number = 540;
+    @property(Number)
+    public screenHeight : number = 1000;
+
     protected start(): void {
-        view.setDesignResolutionSize(540, 1000, ResolutionPolicy.EXACT_FIT)
+        view.setDesignResolutionSize(this.screenWidth, this.screenHeight, ResolutionPolicy.EXACT_FIT)
     }
+    
 }
 
 
